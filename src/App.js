9360bd5e8e209142a93c2ccffe8889cec9791717
yıldız <mesel4461@gmail.com>
2023-06-01 02:07:51 +0300

    as

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
-const App = () => {
-  return <div>App</div>;
-};
+const App = () => (
+  <Router>
+    <Box sx={{ backgroundColor: "#000" }}>
+      Navbar
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" />
+      </Routes>
+    </Box>
+  </Router>
+);
 
 export default App;
