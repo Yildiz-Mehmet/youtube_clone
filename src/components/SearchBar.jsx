2473afb6ea54a683c4e3ff74e7c4ebb08740afe2
yıldız <mesel4461@gmail.com>
2023-06-01 09:31:47 +0300

    as

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Paper, IconButton } from "@mui/material";
+import { SearchIcon } from "@mui/icons-material";
+const SearchBar = () => {
+  return <div>SearchBar</div>;
+};
+
+export default SearchBar;
