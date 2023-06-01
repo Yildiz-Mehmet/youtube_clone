71f1c514489d92fd4168322978cee8ac8d49e492
yıldız <mesel4461@gmail.com>
2023-06-01 15:42:56 +0300

    <z

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Stack, Box } from "@mui/material";
 
 const Videos = ({ videos }) => {
-  console.log(videos);
-
-  return <div>Videos</div>;
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, i) => (
+        <Box key={i}></Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
