795dc25e9387f314979d56bb6d9390f598dbede3
yıldız <mesel4461@gmail.com>
2023-06-01 10:21:00 +0300

    as

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import { Paper, IconButton } from "@mui/material";
-import { SearchIcon } from "@mui/icons-material";
+import SearchIcon from "@mui/icons-material/Search";
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value=""
+        onChange={() => {}}
+      />
+      <IconButton
+        type="submit"
+        sx={{
+          p: "10px",
+          color: "red",
+        }}
+      >
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
 };
 
 export default SearchBar;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Stack } from "@mui/material";
+
+import { categories } from "../utils/constans";
+
+const selectedCategory = "New";
+const Sidebar = () => (
+  <Stack
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
+  >
+    {categories.map((category) => (
+      <button
+        className="category-btn"
+        style={{
+          background: category.name === selectedCategory && "#FC1503",
+          color: "white",
+        }}
+        key={category.name}
+      >
+        <span>{category.icon}</span>
+        <span>{category.name}</span>
+      </button>
+    ))}
+  </Stack>
+);
+
+export default Sidebar;
