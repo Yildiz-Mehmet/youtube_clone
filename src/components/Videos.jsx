a09581fd9cf05a2dd8b6ad91cf2f694b64fdb97d
yıldız <mesel4461@gmail.com>
2023-06-01 21:16:40 +0300

    as

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Stack, Box } from "@mui/material";
+import { VideoCard, ChannelCard } from "../components";
 
 const Videos = ({ videos }) => {
   return (
     <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
       {videos.map((item, i) => (
-        <Box key={i}></Box>
+        <Box key={i}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
       ))}
     </Stack>
   );
